f56ec6d511294bedcf4a440f227bf70c8db2dd3f
Martin Hwang <mhwang@omics.com>
2021-10-19 12:31:14 -0300

    customized virtual keypad

--- src/components/home.js
+++ src/components/home.js
@@ -4,27 +4,34 @@ import { withStyles } from '@material-ui/styles';
 import Keyboard from 'react-virtual-keyboard'
 const styles = {
     root:{
+        '& .ui-keyboard': {
+        },
         '& .ui-keyboard-button': {
             margin: "0",
             padding: "0",
-            background: "lightgrey",
-            width: "3em",
-            height: "3em",
-            borderWidth: "0.5px",
-            lineHeight: "1px"
+            // background: "lightgrey",
+            width: "5em",
+            height: "5em",
+            border: "0.5px solid #cccbcb",
+            borderRadius: "5px",
+            lineHeight: "1px",
+            // backgroundImage: "-webkit-linear-gradient(-90deg, #f6f4f4  0%, #e2e2e4 100%)",
+            backgroundImage: "linear-gradient(-90deg, #d3d3d3 0%, #e2e2e4 100%)",
+            cursor: "pointer",
+            '& hover': {
+                background: "#fff",
+            }
         },
         '& .ui-keyboard-text': {
-            color: "grey"
+            color: "#4d4d4d",
+            fontSize: "1em"
         }
     }
 }
-// onInputChanged = (data) => {
-//     this.setState({ input: data });
-// }
 
-// onInputSubmitted = (data) => {
-//     console.log("Input submitted:", data);
-// }
+onInputSubmitted = (data) => {
+    console.log("Input submitted:", data);
+}
 // this.keyboard.interface.keyaction.enter = (base) => {
 //     // Enter button pressed
 //     // Accepting content, as an example:
@@ -33,6 +40,11 @@ const styles = {
 function Home(props) {
     const [input, setInput] = useState();
     const { classes } = props;
+    const onInputChanged = (data) => {
+        setInput(data);
+        console.log('data:', data)
+    }
+    console.log('input:', input);
     return (
         <Box className={classes.root}>
               <Keyboard 
@@ -40,20 +52,25 @@ function Home(props) {
                 name='keyboard'
                 options={{
                     type:"num",
-                    layout: "num",
+                    layout : 'custom',
+                    customLayout: {
+                        'normal' : ["1 2 3", "4 5 6", "7 8 9", "- 0 {bksp}", "{a} {c} {del}"],
+                        // 'shift'  : ['A B C D E', 'F G H I J', '{shift} {meta1} {a} {c}'],
+                        // 'meta1'  : ['1 2 3 4 5', '6 7 8 9 0', '{shift} {meta1} {a} {c}']
+                    },
                     restrictInput : true, // Prevent keys not in the displayed keyboard from being typed in
                     preventPaste : true,  // prevent ctrl-v and right click
                     autoAccept : true,
-
                     appendLocally: true,
-                    // color: "dark",
+                    color: "dark",
                     // updateOnChange: true,
                     // initialFocus: true,
                     display: {
                         "accept" : "Submit"
                     }
                 }}
-                // onChange={this.onInputChanged}
+                onChange={onInputChanged}
+                
                 // onAccepted={this.onInputSubmitted}
                 // ref={k => this.keyboard = k}
             />
